665cf9999083abbc7a885d2547944ab6a4a0cb74
Anshul Rajput <82425167+LUCKYRAJPUTOP@users.noreply.github.com>
2021-04-24 18:30:29 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -16,7 +16,7 @@ from helpers.errors import DurationLimitError
     & ~ filters.edited
 )
 async def play_(client: Client, message: Message):
-    await message.reply_text("**KINGBOT:** Sorry! I can only be used in groups. \nTry again in a group.")
+    await message.reply_text("**RAJPUTBOT:** Sorry! I can only be used in groups. \nTry again in a group.")
 
 
 @Client.on_message(
@@ -28,12 +28,12 @@ async def play_(client: Client, message: Message):
 async def play(client: Client, message_: Message):
     audio = (message_.reply_to_message.audio or message_.reply_to_message.voice) if message_.reply_to_message else None
 
-    res = await message_.reply_text("**KINGBOT:** 🔄 Processing...")
+    res = await message_.reply_text("**RAJPUTBOT:** 🔄 Processing...")
 
     if audio:
         if round(audio.duration / 60) > DURATION_LIMIT:
             raise DurationLimitError(
-                f"**KINGBOT:** Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {audio.duration / 60} minute(s)"
+                f"**RAJPUTBOT:** Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {audio.duration / 60} minute(s)"
             )
 
         file_name = audio.file_id + audio.file_name.split(".")[-1]
@@ -59,7 +59,7 @@ async def play(client: Client, message_: Message):
                         break
 
         if offset == None:
-            await res.edit_text("**KINGBOT:**❕ You did not give me anything to play.")
+            await res.edit_text("**RAJPUTBOT:**❕ You did not give me anything to play.")
             return
 
         url = text[offset:offset+length]
@@ -73,7 +73,7 @@ async def play(client: Client, message_: Message):
 
     if is_playing:
         position = await sira.add(message_.chat.id, file_path)
-        await res.edit_text(f"**KINGBOT:** #️⃣ Queued at position {position}.")
+        await res.edit_text(f"**RAJPUTBOT:** #️⃣ Queued at position {position}.")
     else:
-        await res.edit_text("**KINGBOT:** ▶️ Playing...")
+        await res.edit_text("**RAJPUTBOT:** ▶️ Playing...")
         tgcalls.pytgcalls.join_group_call(message_.chat.id, file_path, 48000)
